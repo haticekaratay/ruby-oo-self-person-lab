c04a0076178b90fe770ab3bfa02a4aa9bc50b424
Hatice <haticekaraty@gmail.com>
2021-01-30 15:03:07 -0500

    commit tree 5352c07d56aa3351e54b2c6c6fca44384cd7be63

--- lib/person.rb
+++ lib/person.rb
@@ -49,28 +49,29 @@ class Person
     end
 
     def work_out
-        self.happiness += 2
-        self.hygiene -= 3
-        binding.pry
+        self.happiness = @happiness + 2
+        self.hygiene = @hygiene - 3
+        #binding.pry
         return "♪ another one bites the dust ♫"
         
     end
     
     
     def call_friend(friend)
-        @happiness  += 3
+        self.happiness  = @happiness + 3
         friend.happiness += 3
-        if friend == "Felix"
-            return "Hi Felix! It's Stella. How are you?"
-        end
+        "Hi #{friend.name}! It's #{self.name}. How are you?"   
     end
+   #binding.pry
 
     def start_conversation(person,topic)
-        if topic.downcase == "politics"
-            @happiness -= 2
+        if topic == "politics"
+            person.happiness  -= 2
+            self.happiness  -=2 
             return "blah blah partisan blah lobbyist"
-        elsif topic.downcase == "weather"
-            @happiness += 2
+        elsif topic  == "weather"
+            person.happiness += 1
+            self.happiness += 1
             return "blah blah sun blah rain"
         else
             return "blah blah blah blah blah"
